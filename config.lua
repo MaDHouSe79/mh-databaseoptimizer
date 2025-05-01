2ac65f7903a71dc3ee6a43ae62d1a5077e6d5f5c
MaDHouSe <madhouse1979@gmail.com>
2025-05-01 10:22:24 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -7,4 +7,22 @@ Config.IgnoreInventoryNames = {
     ["delivery_coke_owner_stash"] = true,
     ["delivery_meth_stash"] = true,
     ["delivery_meth_owner_stash"] = true,
+    -- Add more as needed
+}
+
+Config.NonActiveDays = 60
+Config.PlayerTables = { 
+    { table = 'players' },
+    { table = 'apartments' },
+    { table = 'bank_accounts' },
+    { table = 'crypto_transactions' },
+    { table = 'phone_invoices' },
+    { table = 'phone_messages' },
+    { table = 'playerskins' },
+    { table = 'player_contacts' },
+    { table = 'player_houses' },
+    { table = 'player_mails' },
+    { table = 'player_outfits' },
+    { table = 'player_vehicles' },
+    -- Add tables as needed
 }
