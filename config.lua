4cde5849c3b6ba69fe3f3f31963dfc7a3ec40619
MaDHouSe <madhouse1979@gmail.com>
2025-08-30 13:58:45 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,27 @@
+
+-- This will ignore inventories that are empty but used.
+Config.IgnoreInventoryNames = {
+    ["delivery_weed_stash"] = true,
+    ["delivery_weed_owner_stash"] = true,
+    ["delivery_coke_stash"] = true,
+    ["delivery_coke_owner_stash"] = true,
+    ["delivery_meth_stash"] = true,
+    ["delivery_meth_owner_stash"] = true,
+}
+
+Config.NonActiveDays = 60
+Config.PlayerTables = { 
+    { table = 'players' },
+    { table = 'apartments' },
+    { table = 'bank_accounts' },
+    { table = 'crypto_transactions' },
+    { table = 'phone_invoices' },
+    { table = 'phone_messages' },
+    { table = 'playerskins' },
+    { table = 'player_contacts' },
+    { table = 'player_houses' },
+    { table = 'player_mails' },
+    { table = 'player_outfits' },
+    { table = 'player_vehicles' },
+    -- Add tables as needed
+}
